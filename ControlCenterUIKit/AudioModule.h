6a7634227a5a9f6b11ec3bf2e4ebb68a79289bd0
KanesBetas <kanebuckthorpe@live.co.uk>
2019-03-20 13:30:11 +0000

    AudioModule

---
+++ ControlCenterUIKit/AudioModule.h
@@ -0,0 +1,4 @@
+#import <ControlCenterUIKit/CCUIVolumeSliderView.h>
+#import <ControlCenterUIKit/CCUIAudioModule.h>
+#import <ControlCenterUIKit/CCUIVolumeSlider.h>
+#import <ControlCenterUIKit/CCUIAudioModuleViewController.h>
